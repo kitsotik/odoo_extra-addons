0f913acd67625ae502c5e3c134ac044710166143
kitsotik <gonchibud@gmail.com>
2024-06-22 14:18:09 -0300

    Actualizacion realizada el $fecha

--- update-repo-remoto.bat
+++ update-repo-remoto.bat
@@ -1,6 +1,7 @@
 git checkout main
 git status
 git add .
-git commit -m "Actualizacion realizada el $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
+$fecha = Get-Date -Format 'yyyy-MM-dd HH:mm:ss'
+git commit -m "Actualizacion realizada el $fecha"
 git push origin 16.0
 
